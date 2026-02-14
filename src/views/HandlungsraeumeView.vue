7ea1de8c60c1d2a6969d50562ed4232d2898cc31
EfaM <git@maria-eva.de>
2026-02-14 22:27:44 +0100

    Bug-Fix: div-p

--- src/views/HandlungsraeumeView.vue
+++ src/views/HandlungsraeumeView.vue
@@ -1,57 +1,55 @@
 <template>
-  <div>
-    <h1>Handlungsräume</h1>
-    <div class="p">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque aut magni sed aspernatur
-      dolore reprehenderit illo vel? Fuga, vel. Quas aut deserunt, voluptatem sunt sint vel beatae
-      deleniti et consequuntur?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
-      voluptatem, nobis dicta saepe illum maxime nihil optio qui soluta eaque voluptatibus
-      asperiores cumque impedit harum laborum debitis, ab eos. Quis fugiat quae nobis expedita et
-      tenetur sit laboriosam qui sequi incidunt ullam eligendi dolorem suscipit odio modi temporibus
-      quasi nostrum, deleniti vero distinctio repudiandae voluptas quam. Provident esse harum quam
-      unde exercitationem, necessitatibus vero enim beatae voluptates voluptatem incidunt fugiat
-      facilis quisquam ullam consequuntur et qui corporis obcaecati repellendus, ex magni corrupti
-      alias adipisci doloribus! Tempora voluptatum quidem eos adipisci delectus iure, sed recusandae
-      reiciendis quibusdam excepturi pariatur obcaecati molestias.
-    </div>
-    <div class="p">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque aut magni sed aspernatur
-      dolore reprehenderit illo vel? Fuga, vel. Quas aut deserunt, voluptatem sunt sint vel beatae
-      deleniti et consequuntur?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
-      voluptatem, nobis dicta saepe illum maxime nihil optio qui soluta eaque voluptatibus
-      asperiores cumque impedit harum laborum debitis, ab eos. Quis fugiat quae nobis expedita et
-      tenetur sit laboriosam qui sequi incidunt ullam eligendi dolorem suscipit odio modi temporibus
-      quasi nostrum, deleniti vero distinctio repudiandae voluptas quam. Provident esse harum quam
-      unde exercitationem, necessitatibus vero enim beatae voluptates voluptatem incidunt fugiat
-      facilis quisquam ullam consequuntur et qui corporis obcaecati repellendus, ex magni corrupti
-      alias adipisci doloribus! Tempora voluptatum quidem eos adipisci delectus iure, sed recusandae
-      reiciendis quibusdam excepturi pariatur obcaecati molestias.
-    </div>
-    <div class="p">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque aut magni sed aspernatur
-      dolore reprehenderit illo vel? Fuga, vel. Quas aut deserunt, voluptatem sunt sint vel beatae
-      deleniti et consequuntur?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
-      voluptatem, nobis dicta saepe illum maxime nihil optio qui soluta eaque voluptatibus
-      asperiores cumque impedit harum laborum debitis, ab eos. Quis fugiat quae nobis expedita et
-      tenetur sit laboriosam qui sequi incidunt ullam eligendi dolorem suscipit odio modi temporibus
-      quasi nostrum, deleniti vero distinctio repudiandae voluptas quam. Provident esse harum quam
-      unde exercitationem, necessitatibus vero enim beatae voluptates voluptatem incidunt fugiat
-      facilis quisquam ullam consequuntur et qui corporis obcaecati repellendus, ex magni corrupti
-      alias adipisci doloribus! Tempora voluptatum quidem eos adipisci delectus iure, sed recusandae
-      reiciendis quibusdam excepturi pariatur obcaecati molestias.
-    </div>
-    <div class="p">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque aut magni sed aspernatur
-      dolore reprehenderit illo vel? Fuga, vel. Quas aut deserunt, voluptatem sunt sint vel beatae
-      deleniti et consequuntur?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
-      voluptatem, nobis dicta saepe illum maxime nihil optio qui soluta eaque voluptatibus
-      asperiores cumque impedit harum laborum debitis, ab eos. Quis fugiat quae nobis expedita et
-      tenetur sit laboriosam qui sequi incidunt ullam eligendi dolorem suscipit odio modi temporibus
-      quasi nostrum, deleniti vero distinctio repudiandae voluptas quam. Provident esse harum quam
-      unde exercitationem, necessitatibus vero enim beatae voluptates voluptatem incidunt fugiat
-      facilis quisquam ullam consequuntur et qui corporis obcaecati repellendus, ex magni corrupti
-      alias adipisci doloribus! Tempora voluptatum quidem eos adipisci delectus iure, sed recusandae
-      reiciendis quibusdam excepturi pariatur obcaecati molestias.
-    </div>
-  </div>
+  <h1>Handlungsräume</h1>
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque aut magni sed aspernatur
+    dolore reprehenderit illo vel? Fuga, vel. Quas aut deserunt, voluptatem sunt sint vel beatae
+    deleniti et consequuntur?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
+    voluptatem, nobis dicta saepe illum maxime nihil optio qui soluta eaque voluptatibus asperiores
+    cumque impedit harum laborum debitis, ab eos. Quis fugiat quae nobis expedita et tenetur sit
+    laboriosam qui sequi incidunt ullam eligendi dolorem suscipit odio modi temporibus quasi
+    nostrum, deleniti vero distinctio repudiandae voluptas quam. Provident esse harum quam unde
+    exercitationem, necessitatibus vero enim beatae voluptates voluptatem incidunt fugiat facilis
+    quisquam ullam consequuntur et qui corporis obcaecati repellendus, ex magni corrupti alias
+    adipisci doloribus! Tempora voluptatum quidem eos adipisci delectus iure, sed recusandae
+    reiciendis quibusdam excepturi pariatur obcaecati molestias.
+  </p>
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque aut magni sed aspernatur
+    dolore reprehenderit illo vel? Fuga, vel. Quas aut deserunt, voluptatem sunt sint vel beatae
+    deleniti et consequuntur?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
+    voluptatem, nobis dicta saepe illum maxime nihil optio qui soluta eaque voluptatibus asperiores
+    cumque impedit harum laborum debitis, ab eos. Quis fugiat quae nobis expedita et tenetur sit
+    laboriosam qui sequi incidunt ullam eligendi dolorem suscipit odio modi temporibus quasi
+    nostrum, deleniti vero distinctio repudiandae voluptas quam. Provident esse harum quam unde
+    exercitationem, necessitatibus vero enim beatae voluptates voluptatem incidunt fugiat facilis
+    quisquam ullam consequuntur et qui corporis obcaecati repellendus, ex magni corrupti alias
+    adipisci doloribus! Tempora voluptatum quidem eos adipisci delectus iure, sed recusandae
+    reiciendis quibusdam excepturi pariatur obcaecati molestias.
+  </p>
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque aut magni sed aspernatur
+    dolore reprehenderit illo vel? Fuga, vel. Quas aut deserunt, voluptatem sunt sint vel beatae
+    deleniti et consequuntur?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
+    voluptatem, nobis dicta saepe illum maxime nihil optio qui soluta eaque voluptatibus asperiores
+    cumque impedit harum laborum debitis, ab eos. Quis fugiat quae nobis expedita et tenetur sit
+    laboriosam qui sequi incidunt ullam eligendi dolorem suscipit odio modi temporibus quasi
+    nostrum, deleniti vero distinctio repudiandae voluptas quam. Provident esse harum quam unde
+    exercitationem, necessitatibus vero enim beatae voluptates voluptatem incidunt fugiat facilis
+    quisquam ullam consequuntur et qui corporis obcaecati repellendus, ex magni corrupti alias
+    adipisci doloribus! Tempora voluptatum quidem eos adipisci delectus iure, sed recusandae
+    reiciendis quibusdam excepturi pariatur obcaecati molestias.
+  </p>
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque aut magni sed aspernatur
+    dolore reprehenderit illo vel? Fuga, vel. Quas aut deserunt, voluptatem sunt sint vel beatae
+    deleniti et consequuntur?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
+    voluptatem, nobis dicta saepe illum maxime nihil optio qui soluta eaque voluptatibus asperiores
+    cumque impedit harum laborum debitis, ab eos. Quis fugiat quae nobis expedita et tenetur sit
+    laboriosam qui sequi incidunt ullam eligendi dolorem suscipit odio modi temporibus quasi
+    nostrum, deleniti vero distinctio repudiandae voluptas quam. Provident esse harum quam unde
+    exercitationem, necessitatibus vero enim beatae voluptates voluptatem incidunt fugiat facilis
+    quisquam ullam consequuntur et qui corporis obcaecati repellendus, ex magni corrupti alias
+    adipisci doloribus! Tempora voluptatum quidem eos adipisci delectus iure, sed recusandae
+    reiciendis quibusdam excepturi pariatur obcaecati molestias.
+  </p>
 </template>
