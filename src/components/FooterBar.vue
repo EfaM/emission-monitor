77b7413303a4c98bab63c0683d7fa953afa990b7
EfaM <git@maria-eva.de>
2026-02-22 17:27:52 +0100

    Footer optisch verbessert

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -5,13 +5,15 @@ const nav = navConfigData.FooterItems
 </script>
 
 <template>
-  <footer>
-    <ul class="nav flex-column">
-      <li v-for="navItem in nav" :key="navItem.navPosition" class="nav-item">
-        <RouterLink :to="'/' + navItem.urlpath" class="nav-link" active-class="active">
-          {{ navItem.linkTitle }}
-        </RouterLink>
-      </li>
-    </ul>
+  <footer class="navbar navbar-expand-sm my-4 bg-white border-top">
+    <div class="container-fluid">
+        <ul class="navbar-nav me-auto mb-2">
+          <li v-for="navItem in nav" :key="navItem.navPosition" class="nav-item">
+            <RouterLink :to="'/' + navItem.urlpath" class="nav-link" active-class="active">
+              {{ navItem.linkTitle }}
+            </RouterLink>
+          </li>
+       </ul>
+    </div>
   </footer>
 </template>
